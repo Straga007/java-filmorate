cd5a9d6bf83ee4cf9ec5c0f3cd81deb91221dc80
Straga07 <Straga07@yandex.ru>
2023-05-26 13:29:52 +0300

    separated UserService

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -11,23 +11,28 @@ import java.util.Set;
 @Service
 public class UserService {
     private final UserStorage userStorage;
+
     @Autowired
     public UserService(UserStorage userStorage) {
         this.userStorage = userStorage;
     }
-    public void addFriend(int userId,int friendId){
+
+    public void addFriend(int userId, int friendId) {
         userStorage.findUser(userId).setFriend(friendId);
         userStorage.findUser(friendId).setFriend(userId);
     }
-    public Set<Integer> getAllFriend(int userId){
+
+    public Set<Integer> getAllFriend(int userId) {
         return userStorage.findUser(userId).getFriends();
     }
-    public void delFriend(int userId,int friendId){
+
+    public void delFriend(int userId, int friendId) {
         userStorage.findUser(userId).delFriend(friendId);
         userStorage.findUser(friendId).delFriend(userId);
     }
-    public Collection<User> getCommonFriends(int userId, int friendId){
 
-        return userStorage.findCommonFriends(userId,friendId);
+    public Collection<User> getCommonFriends(int userId, int friendId) {
+
+        return userStorage.findCommonFriends(userId, friendId);
     }
 }
