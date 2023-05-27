76554d8edc33cf39d87609a1cd727f61b93b7f9e
Straga07 <Straga07@yandex.ru>
2023-05-27 13:36:51 +0300

    update: ??

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
         log.info("Пользователь c id {} добавил в друзья пользователя с id {}", userId, friendId);
     }
 
-    @GetMapping("users/{id}/friends")
+    @GetMapping("users/{id}/friends/")
     public List<User> getAllFriend(@PathVariable int id) {
         return userService.getAllFriend(id);
     }
