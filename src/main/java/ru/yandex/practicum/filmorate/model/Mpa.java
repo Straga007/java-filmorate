a5170ba9a76773ac80466279cf440e3c0c352ae6
Straga07 <Straga07@yandex.ru>
2023-08-17 19:37:30 +0300

    feat: fix Unused import

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Mpa  {
+public class Mpa {
 
     private int id;
     private String name;
