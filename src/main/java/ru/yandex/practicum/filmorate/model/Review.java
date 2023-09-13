b8fbaacb5273aed52c5ac840251faeba60a799a5
Straga07 <Straga07@yandex.ru>
2023-09-14 02:37:33 +0300

    update: check and controllers + methods + DB ( is 87 string working  ?).

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Review {
-    long reviewId;
+    int reviewId;
     String content;
     boolean isPositive;
     int userId;
