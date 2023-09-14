7614d700fd10da099cf6fbfbc0100da10b487fa2
Straga07 <Straga07@yandex.ru>
2023-09-14 14:47:27 +0300

    update: ... 61 warnings isPositive test

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -4,14 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Review {
     int reviewId;
     String content;
-    boolean isPositive;
     int userId;
     int filmId;
     int useful;
+    boolean isPositive;
+
+    public boolean getIsPositive() {
+        return isPositive;
+    }
 }
