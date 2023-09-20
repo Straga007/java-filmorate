aa5e65fe3d65a52e6ae1373fc83f5e73c1f41641
dmt-100 <dmt-100@yandex.ru>
2023-09-20 14:49:33 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
